05d1adc9be440cb1a36e4e857a740fd7087855f8
Robert Hooper <solarbinarysystems@gmail.com>
2018-03-03 13:14:36 -0800

    moved css and frontScript

---
+++ scripts/frontScript.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCHPr2zlrS33ebdetDjYXqa9cDfzsjXt9c",
+    authDomain: "pricing-tool-e606f.firebaseapp.com",
+    databaseURL: "https://pricing-tool-e606f.firebaseio.com",
+    projectId: "pricing-tool-e606f",
+    storageBucket: "",
+    messagingSenderId: "810044611886"
+  };
+  firebase.initializeApp(config);
