a4b6aee5de56fc0d04a5e58b9d538478aadfa753
tj Bains <tjbains1943@yahoo.com>
2018-03-06 19:57:27 -0800

    added first button

--- scripts/frontScript.js
+++ scripts/frontScript.js
@@ -1,32 +1,40 @@
+$(document).ready(function() {
+  google.charts.load("current", { packages: ["bar"] });
+  google.charts.setOnLoadCallback(drawStuff);
 
-      google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawStuff);
+  function drawStuff() {
+    var data = new google.visualization.arrayToDataTable([
+      ["Price", "Amount"],
+      ["Panties", 44],
+      ["Non", 31],
+      ["Null", 12],
+      ["True", 10],
+      ["Falso", 3],
+    ]);
 
-      function drawStuff() {
-        var data = new google.visualization.arrayToDataTable([
-          ['Price', 'Amount'],
-          ["Panties", 44],
-          ["Non", 31],
-          ["Null", 12],
-          ["True", 10],
-          ['Falso', 3]
-        ]);
+    var options = {
+      title: "Chess opening moves",
+      width: 900,
+      legend: { position: "none" },
+      chart: {
+        title: "Pricing Tool",
+        subtitle: "Category Graph",
+      },
+      bars: "horizontal", // Required for Material Bar Charts.
+      axes: {
+        x: {
+          0: { side: "top", label: "Amount" }, // Top x-axis.
+        },
+      },
+      bar: { groupWidth: "90%" },
+    };
 
-        var options = {
-          title: 'Chess opening moves',
-          width: 900,
-          legend: { position: 'none' },
-          chart: { title: 'Pricing Tool',
-                   subtitle: 'Category Graph' },
-          bars: 'horizontal', // Required for Material Bar Charts.
-          axes: {
-            x: {
-              0: { side: 'top', label: 'Amount'} // Top x-axis.
-            }
-          },
-          bar: { groupWidth: "90%" }
-        };
+    var chart = new google.charts.Bar(document.getElementById("top_x_div"));
+    chart.draw(data, options);
+  }
 
-        var chart = new google.charts.Bar(document.getElementById('top_x_div'));
-        chart.draw(data, options);
-      };
+$(".button-primary").on("click", function(event) {
+  event.preventDefault();
+  $("#top_x_div").toggle();
+})
+});
