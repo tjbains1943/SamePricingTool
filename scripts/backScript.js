2154631ec65ae132b2ede6f5e3328b36e9112728
tj Bains <tjbains1943@yahoo.com>
2018-03-06 19:02:37 -0800

    fixed Maureen firebase

---
+++ scripts/backScript.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCeu8LNzpEl4vmP-_Gm4pRL04krzERMLDs",
+  authDomain: "pricingtool-7ba32.firebaseapp.com",
+  databaseURL: "https://pricingtool-7ba32.firebaseio.com",
+  projectId: "pricingtool-7ba32",
+  storageBucket: "",
+  messagingSenderId: "53201548843",
+};
