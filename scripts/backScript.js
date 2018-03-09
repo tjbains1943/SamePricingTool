ad87e1b7046ec72bb16d92faaa04f87d3b4a96e7
Robert Hooper <solarbinarysystems@gmail.com>
2018-03-08 20:48:50 -0800

    This fixes the backScript ajax and wrapper function getData. Test call to getData

--- scripts/backScript.js
+++ scripts/backScript.js
@@ -52,102 +52,48 @@ function getData() {
   console.log(queryURL);
   console.log(category);
   console.log(item);
-  
+
+
   $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (response) {
-        result = JSON.parse(response);
-        //   console.log(result);
-        //   console.log(result.findCompletedItemsResponse[0].searchResult[0].item.length);
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    result = JSON.parse(response);
 
-        for (var i = 0; i < result.findCompletedItemsResponse[0].searchResult[0].item.length; i++) {
+    //   console.log(result);
+    //   console.log(result.findCompletedItemsResponse[0].searchResult[0].item.length);
 
-          var newRow = $("<tr>");
-          var picture = $("<td>").html("<img src=" + result.findCompletedItemsResponse[0].searchResult[0].item[i].galleryURL + "alt='img'>");
-          var Title = $("<td>").text(result.findCompletedItemsResponse[0].searchResult[0].item[i].title);
-          var Price = $("<td>").text(result.findCompletedItemsResponse[0].searchResult[0].item[i].sellingStatus[0].currentPrice[0].__value__);
+    for (var i = 0; i < result.findCompletedItemsResponse[0].searchResult[0].item.length; i++) {
 
-          newRow.append(picture);
-          newRow.append(Title);
-          newRow.append(Price);
+      var newRow = $("<tr>");
+      var picture = $("<td>").html("<img src=" + result.findCompletedItemsResponse[0].searchResult[0].item[i].galleryURL + "alt='img'>");
+      var Title = $("<td>").text(result.findCompletedItemsResponse[0].searchResult[0].item[i].title);
+      var Price = $("<td>").text(result.findCompletedItemsResponse[0].searchResult[0].item[i].sellingStatus[0].currentPrice[0].__value__);
 
-          if (result.findCompletedItemsResponse[0].searchResult[0].item[i].shippingInfo[0].hasOwnProperty('shippingServiceCost')) {
+      newRow.append(picture);
+      newRow.append(Title);
+      newRow.append(Price);
 
-            Shipping = $("<td>").text(result.findCompletedItemsResponse[0].searchResult[0].item[i].shippingInfo[0].shippingServiceCost[0].__value__);
+      if (result.findCompletedItemsResponse[0].searchResult[0].item[i].shippingInfo[0].hasOwnProperty('shippingServiceCost')) {
 
-            totalNumber = Number(result.findCompletedItemsResponse[0].searchResult[0].item[i].sellingStatus[0].currentPrice[0].__value__) + Number(result.findCompletedItemsResponse[0].searchResult[0].item[i].shippingInfo[0].shippingServiceCost[0].__value__);
-          } else {
+        Shipping = $("<td>").text(result.findCompletedItemsResponse[0].searchResult[0].item[i].shippingInfo[0].shippingServiceCost[0].__value__);
 
-            Shipping = $("<td>").text("$0.00");
+        totalNumber = Number(result.findCompletedItemsResponse[0].searchResult[0].item[i].sellingStatus[0].currentPrice[0].__value__) + Number(result.findCompletedItemsResponse[0].searchResult[0].item[i].shippingInfo[0].shippingServiceCost[0].__value__);
+      } else {
 
-            totalNumber = Number(result.findCompletedItemsResponse[0].searchResult[0].item[i].sellingStatus[0].currentPrice[0].__value__) + 0;
-          }
+        Shipping = $("<td>").text("$0.00");
 
-          var total = $("<td>").text(totalNumber.toFixed(2));
-
-          newRow.append(Shipping);
-          newRow.append(total);
-          body.append(newRow);
-
-          if (totalNumber.toFixed(2) > Number(highest)) {
-            console.log(highest);
-            console.log(totalNumber);
-            highest = totalNumber.toFixed(2);
-            console.log("New high price: " + totalNumber.toFixed(2));
-          }
-
-          if (totalNumber.toFixed(2) < Number(lowest)) {
-            console.log(lowest);
-            console.log(totalNumber);
-            lowest = totalNumber.toFixed(2);
-            console.log("New low price: " + totalNumber.toFixed(2));
-          }
-
-          totals.push(totalNumber);
-          totalsRound.push(Math.round(totalNumber));
-          //   console.log(totalNumber);
-          runningTally += totalNumber;
-          //   console.log(runningTally.toFixed(2));
-        }
-
-        console.log(totals);
-        console.log(totalsRound);
-        totals.sort(function (a, b) {
-          return a - b;
-        });
-        totalsRound.sort(function (a, b) {
-          return a - b;
-        });
-        console.log(totals);
-        console.log(totalsRound);
-
-        createFreq(totalsRound);
-
-        var lowMiddle = Math.floor((totals.length - 1) / 2);
-        console.log("lowMiddle " + lowMiddle);
-        var highMiddle = Math.ceil((totals.length - 1) / 2);
-        console.log("highMiddle " + highMiddle);
-        var mean = runningTally.toFixed(2) / result.findCompletedItemsResponse[0].searchResult[0].item.length;
-        console.log("mean " + mean);
-        var median = (totals[lowMiddle] + totals[highMiddle]) / 2;
-        console.log("median " + median);
-
-
-        var importantRow = $("<tr>");
-
-        var priceRange = $("<td>").text(lowest + "-" + highest);
-        var meanPrice = $("<td>").text(mean.toFixed(2));
-        var medianPrice = $("<td>").text(median.toFixed(2));
-        var modePrice = $("<td>").text(modes(totals));
-        var roundedMode = $("<td>").text(modes(totalsRound));
-        var numberOfItems = $("<td>").text(result.findCompletedItemsResponse[0].searchResult[0].item.length);
-
-        importantRow.append(numberOfItems);
-        importantRow.append(priceRange);
-        importantRow.append(meanPrice);
-        importantRow.append(medianPrice);
-        importantRow.append(modePrice);
-        importantRow.append(roundedMode);
-        head.append(importantRow);
+        totalNumber = Number(result.findCompletedItemsResponse[0].searchResult[0].item[i].sellingStatus[0].currentPrice[0].__value__) + 0;
       }
+
+      var total = $("<td>").text(totalNumber.toFixed(2));
+
+      newRow.append(Shipping);
+      newRow.append(total);
+      body.append(newRow);
+    }
+  });
+}
+
+// TODO fix this hack, this belongs in a button hndlr on frontscript:
+getData();
